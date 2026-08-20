5a7640aa77758d23174cd6a2593c565726ecd001
Akshay0835 <akshaypratapsingh4219@gmail.com>
2026-08-20 16:23:47 +0530

    Time: 4 ms (5.48%), Space: 48.8 MB (21.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -6,11 +6,16 @@ class Solution {
             return;
         }
         String letter=map[digit.charAt(index)-'0'];
-        for(int i=0;i<letter.length();i++){
-            str+=letter.charAt(i);
-            solve(digit,str,index+1,ans);
-            str=str.substring(0,str.length()-1);
+        solve2(digit,str,index,ans,letter,0);
+    }
+    static void solve2(String digit,String str,int index,List<String> ans,String letter,int j){
+        if (j == letter.length()) {
+            return;
         }
+        str += letter.charAt(j);
+        solve(digit, str, index + 1, ans);
+        str = str.substring(0, str.length() - 1);
+        solve2(digit, str, index, ans, letter, j + 1);
     }
     public List<String> letterCombinations(String digits) {
         List<String> ans=new ArrayList<>();

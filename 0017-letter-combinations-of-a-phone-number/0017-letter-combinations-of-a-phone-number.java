c6e8a997ecffe0b1081aaa4ef8eb744f55c1489f
Akshay0835 <akshaypratapsingh4219@gmail.com>
2026-08-31 23:50:06 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.72%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,44 +1,22 @@
 class Solution {
-
-    List<String> ans = new ArrayList<>();
-
-    String mp[] = {
-        "", "", "abc", "def", "ghi",
-        "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
-
-    void solve(String digits, int index, int letterIndex, StringBuilder temp) {
-
-        if (index == digits.length()) {
-            ans.add(temp.toString());
+    static void solve(String digits,int index,String map[],List<String> ans,StringBuilder output){
+        if(index>=digits.length()){
+            ans.add(output.toString());
             return;
         }
-
-        String letters = mp[digits.charAt(index) - '0'];
-
-        if (letterIndex == letters.length()) {
-            return;
+        int val=digits.charAt(index)-'0';
+        String mapped=map[val];
+        for(int i=0;i<mapped.length();i++){
+            output.append(mapped.charAt(i));
+            solve(digits,index+1,map,ans,output);
+            output.deleteCharAt(output.length()-1);
         }
-
-        temp.append(letters.charAt(letterIndex));
-
-        solve(digits, index + 1, 0, temp);
-
-        temp.deleteCharAt(temp.length() - 1);
-
-        solve(digits, index, letterIndex + 1, temp);
     }
-
     public List<String> letterCombinations(String digits) {
-
-        if (digits.length() == 0) {
-            return new ArrayList<>();
-        }
-
-        StringBuilder temp = new StringBuilder();
-
-        solve(digits, 0, 0, temp);
-
+        String map[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> ans=new ArrayList<>();
+        StringBuilder output=new StringBuilder();
+        solve(digits,0,map,ans,output);
         return ans;
     }
 }
